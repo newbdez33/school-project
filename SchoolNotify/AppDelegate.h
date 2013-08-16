2f509405f614f45da6a38a48e38a0f1ed26778bd
Jacky <newbdez33@gmail.com>
2013-08-16 10:22:39 +0800

    Initial Commit

---
+++ SchoolNotify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolNotify
+//
+//  Created by Jack on 16/08/13.
+//  Copyright (c) 2013 Salmonapps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ SchoolNotify/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SchoolNotify
+//
+//  Created by Jack on 16/08/13.
+//  Copyright (c) 2013 Salmonapps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
