c6e7ea601056ffcd2129d11b50db73eceeaad26a
Jacky <newbdez33@gmail.com>
2013-08-25 08:59:51 +0800

    add user check in mockup api

--- api_mockup/login.php
+++ api_mockup/login.php
@@ -6,5 +6,10 @@ $ret['error_msg'] = "测试错误";
 $ret["role_id"] = 3;
 $ret["school_id"] = 1;
 
+if ($_REQUEST['username']!="a") {
+	$ret['success'] = false;
+	$ret['error_msg'] = "用户名或密码错误";
+}
+
 echo json_encode($ret);
 
